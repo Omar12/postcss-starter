10bf2f09136ac5b63fe324fa50200c1cc73730c0
Omar Martinez <this.is.omartinez@gmail.com>
2015-04-27 22:31:13 -0700

    gulp cssnext edit

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,12 @@ var autoprefixer = require('autoprefixer-core');
 var mqpacker = require('css-mqpacker');
 var csswring = require('csswring');
 
-gulp.task('css', function () {
-    var processors = [
-        autoprefixer({browsers: ['last 2 versions']}),
-        mqpacker,
-        csswring
-    ];
-    return gulp.src('./src/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./build'));
+var cssnext = require("gulp-cssnext")
+
+gulp.task("stylesheets", function() {
+  gulp.src("src/stylesheets/index.css")
+    .pipe(cssnext({
+        compress: true
+    }))
+    .pipe(gulp.dest("./dist/stylesheets"))
 });
